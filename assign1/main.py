a0bbf2275da48dbba1f858f752f48c17ca6fa775
Chris Lin <chris4540@gmail.com>
2019-04-04 15:28:11 +0200

    Add traning code

--- assign1/main.py
+++ assign1/main.py
@@ -8,15 +8,12 @@ if __name__ == '__main__':
     # get the training data
     train_data = load_batch("cifar-10-batches-py/data_batch_1")
 
-    # network = OneLayerNetwork(10, 20, lambda_=0.0)
     X_mat = train_data["pixel_data"]
-    y_mat = train_data["onehot_labels"]
+    Y_mat = train_data["onehot_labels"]
 
-    # # p = network.evaluate(X_mat)
+    nclass = Y_mat.shape[0]
+    ndim = X_mat.shape[0]
+    ann = OneLayerNetwork(nclass, ndim)
 
-    # # cost = network.compute_cost(X_mat, y_mat)
-    # grad_W, grad_b = network.compute_grad(X_mat[:20, :5], y_mat[:, :5])
-    # grad_W2, grad_b2 = network.compute_grads_num(X_mat[:20, :5], y_mat[:, :5])
-
-    # print("Is grad_W all close:", np.allclose(grad_W, grad_W2))
-    # print("Is grad_b all close:", np.allclose(grad_b, grad_b2))
+    ann.set_train_data(X_mat, Y_mat)
+    ann.train()
--- assign1/one_layer_ann.py
+++ assign1/one_layer_ann.py
@@ -5,16 +5,17 @@ import numpy as np
 
 class OneLayerNetwork:
 
-    def __init__(self, nclass, ndim, lambda_=None):
+    n_batch = 100
+    eta = 0.01
+    n_epochs = 40
+
+    def __init__(self, nclass, ndim, lambda_=0.0):
         """
         Args:
             nclass: the number of classes
             ndim: the size of the input vector (X)
         """
-        if lambda_ is None:
-            self.lambda_ = 0.01
-        else:
-            self.lambda_ = lambda_
+        self.lambda_ = lambda_
 
         self.nclass = nclass
         self.ndim = ndim
@@ -27,8 +28,52 @@ class OneLayerNetwork:
         self.W_mat = sigma * np.random.randn(self.nclass, self.ndim)
         self.b_vec = sigma * np.random.randn(self.nclass, 1)
 
-    def train(self):
+    def set_train_data(self, X_train, Y_train):
+        # copy the training set
+        self.X_train = np.copy(X_train)
+        self.Y_train = np.copy(Y_train)
+
+    def set_valid_data(self, X_valid, Y_valid):
         pass
+
+    def train(self):
+
+
+        for iter_ in range(self.n_epochs):
+            # shuffle the samples
+            if True:
+                idx = np.random.rand(self.X_train.shape[1]).argsort()
+                X_train = np.take(self.X_train, idx, axis=1)
+                Y_train = np.take(self.Y_train, idx, axis=1)
+            else:
+                X_train = self.X_train
+                Y_train = self.Y_train
+
+            # mini-batch training
+            self._mini_batch_train(X_train, Y_train)
+
+            # calcualte the cost function
+            cost = self.compute_cost(X_train, Y_train)
+            print("Iteration %d: Cost = %f" % (iter_, cost))
+
+    def _mini_batch_train(self, X_train, Y_train):
+        """
+        Perform Mini batch gradient descent for one epoch
+        """
+        # train with mini-batch
+        n_data = X_train.shape[1]
+        for j in range(n_data // self.n_batch):
+            j_s = j*self.n_batch
+            j_e = (j+1)*self.n_batch
+            X_batch = X_train[:, j_s:j_e]
+            Y_batch = Y_train[:, j_s:j_e]
+
+            # get the gradient of W_mat and b_vec
+            grad_W, grad_b = self.compute_grad(X_batch, Y_batch)
+
+            # update the params
+            self.W_mat = self.W_mat - self.eta * grad_W
+            self.b_vec = self.b_vec - self.eta * grad_b
 
     def evaluate(self, X_mat):
         """
