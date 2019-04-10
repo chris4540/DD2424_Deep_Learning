bfe440cc5598515a4e8f561bbedc6ac521643564
Chris Lin <chris4540@gmail.com>
2019-04-10 23:20:07 +0200

    Update fortran code

--- assign1/lib_ann/ann_fortran.f90
+++ assign1/lib_ann/ann_fortran.f90
@@ -1,15 +1,22 @@
 function softmax(x, axis) result(ret)
     implicit none
-    real(kind=8), dimension(:, :) :: x
-    integer(kind=4) :: axis
+    real(kind=8), dimension(:, :), intent(in) :: x
+    integer(kind=4), intent(in), optional :: axis
     real(kind=8), dimension(size(x,1), size(x,2)) :: ret
     ! local var
     real(kind=8), dimension(size(x,1), size(x,2)) :: exp_prob
+    integer(kind=4) :: axis_
     integer :: i
 
+    if(present(axis))then
+        axis_ = axis
+    else
+        axis_ = 0
+    endif
+    ! ========================================================
     exp_prob = exp(x)
 
-    if (axis == 0) then
+    if (axis_ == 0) then
         ! follow the python convension
         do i = 1, ubound(exp_prob, 2)
             ret(:, i) = exp_prob(:, i) / sum(exp_prob(:, i))
