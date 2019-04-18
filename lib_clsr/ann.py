37cbd1bd2b387ad100f5a02f7800d5cf2dfcfe55
Chris Lin <chris4540@gmail.com>
2019-04-18 21:39:35 +0200

    Update the functions

--- lib_clsr/ann.py
+++ lib_clsr/ann.py
@@ -51,21 +51,19 @@ def evaluate_classifier_klayer(X_mat, Y_mat, W_mats, b_vecs):
     """
     h_mat = X_mat
     h_mats = [h_mat]
-    s_mats = [None]
     for W_mat, b_vec in zip(W_mats, b_vecs):
         s_mat = W_mat.dot(h_mat) + b_vec
         h_mat = np.maximum(s_mat, 0)
         #
         h_mats.append(h_mat)
-        s_mats.append(s_mat)
 
     # calculate the prob.
     p_mat = softmax(s_mat, axis=0)
-    return p_mat, h_mats, s_mats
+    return p_mat, h_mats
 
 def compute_cost_klayer(X_mat, Y_mat, W_mats, b_vecs, lambda_):
     n_data = X_mat.shape[1]
-    p_mat, _, _ = evaluate_classifier_klayer(X_mat, Y_mat, W_mats, b_vecs)
+    p_mat, _ = evaluate_classifier_klayer(X_mat, Y_mat, W_mats, b_vecs)
     cross_entro = -np.log(np.sum(Y_mat*p_mat, axis=0))
     cost = (np.sum(cross_entro) / n_data)
     for W_mat in W_mats:
@@ -77,17 +75,16 @@ def compute_gradients_klayer(X_mat, Y_mat, W_mats, b_vecs, lambda_):
     n_layer = len(W_mats)
 
     # forward pass
-    p_mat, h_mats, s_mats = evaluate_classifier_klayer(X_mat, Y_mat, W_mats, b_vecs)
+    p_mat, h_mats = evaluate_classifier_klayer(X_mat, Y_mat, W_mats, b_vecs)
 
     grad_Ws = [None] * len(W_mats)
     grad_bs = [None] * len(b_vecs)
     # ===============================================
     # start to compute the gradient
-    g_mat = -(Y_mat - p_mat)
+    g_mat = -Y_mat + p_mat
 
     for l in range(n_layer, 0, -1):
         h_mat = h_mats[l-1]
-        s_mat = s_mats[l-1]
         W_mat = W_mats[l-1]
         #
         grad_b = np.mean(g_mat, axis=1)[:, np.newaxis]
@@ -98,9 +95,9 @@ def compute_gradients_klayer(X_mat, Y_mat, W_mats, b_vecs, lambda_):
         grad_bs[l-1] = grad_b
 
         # update g_mat
-        if l != 1:
+        if l != 1: # do not need to update at the last loop
             g_mat = W_mat.T.dot(g_mat)
-            g_mat = g_mat * (s_mat > 0)
+            g_mat = g_mat * (h_mat > 0)
 
 
     return grad_Ws, grad_bs
--- tests/utils.py
+++ tests/utils.py
@@ -71,3 +71,91 @@ def compute_grad_central_diff(X_mat, Y_mat, W_mat, b_vec, lambda_, cost_fun):
         W_mat[idx] = w_old
 
     return (grad_W, grad_b)
+
+def compute_grad_klayers_fwd_diff(X_mat, Y_mat, W_mats, b_vecs, lambda_, cost_fun):
+    """
+    """
+    h = 1e-6
+    h_inv = 1.0 / h
+
+    cost = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+
+    grad_Ws = list()
+    grad_bs = list()
+    for W_mat, b_vec in zip(W_mats, b_vecs):
+
+        # -------------
+        # grad_b
+        # -------------
+        grad_b = np.zeros(b_vec.shape)
+        for idx in np.ndindex(b_vec.shape):
+            b_old = b_vec[idx]
+
+            b_vec[idx] = b_old + h
+            new_cost = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+            grad_b[idx] = (new_cost - cost) * h_inv
+            # put back the old value
+            b_vec[idx] = b_old
+
+        grad_bs.append(grad_b)
+        # -------------
+        # grad_W
+        # -------------
+        grad_W = np.zeros(W_mat.shape)
+        for idx in np.ndindex(W_mat.shape):
+            w_old = W_mat[idx]
+
+            W_mat[idx] = w_old + h
+            new_cost = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+            grad_W[idx] = (new_cost - cost) * h_inv
+            W_mat[idx] = w_old
+
+        grad_Ws.append(grad_W)
+
+    return (grad_Ws, grad_bs)
+
+def compute_grad_klayers_cent_diff(X_mat, Y_mat, W_mats, b_vecs, lambda_, cost_fun):
+    """
+    """
+    h = 1e-6
+    h_inv = 1.0 / h
+
+    grad_Ws = list()
+    grad_bs = list()
+    for W_mat, b_vec in zip(W_mats, b_vecs):
+
+        # -------------
+        # grad_b
+        # -------------
+        grad_b = np.zeros(b_vec.shape)
+        for idx in np.ndindex(b_vec.shape):
+            b_old = b_vec[idx]
+
+            b_vec[idx] = b_old + h
+            c1 = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+            b_vec[idx] = b_old - h
+            c2 = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+            grad_b[idx] = (c1 - c2) * h_inv * 0.5
+            # put back the old value
+            b_vec[idx] = b_old
+
+        grad_bs.append(grad_b)
+        # -------------
+        # grad_W
+        # -------------
+        grad_W = np.zeros(W_mat.shape)
+        for idx in np.ndindex(W_mat.shape):
+            w_old = W_mat[idx]
+
+            W_mat[idx] = w_old + h
+            c1 = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+
+            W_mat[idx] = w_old - h
+            c2 = cost_fun(X_mat, Y_mat, W_mats, b_vecs, lambda_)
+
+            grad_W[idx] = (c1 - c2) * h_inv * 0.5
+            W_mat[idx] = w_old
+
+        grad_Ws.append(grad_W)
+
+    return (grad_Ws, grad_bs)
