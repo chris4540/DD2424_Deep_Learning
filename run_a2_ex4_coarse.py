07109a9930fb7e6000b23076167d00d53288bb54
Chris Lin <chris4540@gmail.com>
2019-04-24 02:28:44 +0200

    Add coarse search result

--- run_a2_ex4_coarse.py
+++ run_a2_ex4_coarse.py
@@ -22,7 +22,7 @@ def get_performance(train_data, valid_data, params, lambda_, n_try=3):
     for _ in range(n_try):
         clsr.fit(train_data["pixel_data"].T, train_data["labels"])
         score = clsr.score(valid_data["pixel_data"].T, valid_data["labels"])
-        print("Accuracy: {}".format(score))
+        print("Lambda: {};Accuracy: {}".format(lambda_, score))
         scores.append(score)
 
     # ===================================
@@ -32,13 +32,14 @@ def get_performance(train_data, valid_data, params, lambda_, n_try=3):
     ret['mean_score'] = np.mean(scores)
     return ret
 
-def save_result(results):
-    with open("a2_ex4_coarse_search.json", 'w') as f:
+def save_result(results, json_name):
+    with open(json_name, 'w') as f:
         json.dump(results, f, indent=2)
 
 
 if __name__ == "__main__":
     n_search = 20
+    search_type = "fine1"
     # ===============================================================
     # READ DATA
     merged_data = get_all_train_data("cifar-10-batches-py")
@@ -56,7 +57,7 @@ if __name__ == "__main__":
         "stop_overfit": False,
         "n_epochs": n_epochs,
         "n_batch": n_batch,
-        "verbose": True,
+        "verbose": False,
         "record_training": False,
         "lrate_scheme": {
             "scheme": "cyclic",
@@ -65,10 +66,18 @@ if __name__ == "__main__":
         }
     }
 
+    if search_type == "coarse":
+        l_min, l_max = -5, -1
+    elif search_type == "fine1":
+        l_min, l_max = -4.5, -3.5
+    elif search_type == "fine2":
+        l_min, l_max = -3, -2
+    result_json = "a2_ex4_%s_search.json" % search_type
+
     results = list()
     for _ in range(n_search):
-        lambda_ = draw_lambda_val(-5, -1)
+        lambda_ = draw_lambda_val(l_min, l_max)
         ret = get_performance(train_data, valid_data, params, lambda_)
         results.append(ret)
         # dump a json whenever one search finish, for resuming the search process
-        save_result(results)
+        save_result(results, result_json)
