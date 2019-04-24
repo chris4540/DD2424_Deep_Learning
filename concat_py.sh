c07ac80ebf3041a4cbca14e9bb8d1fa586a4888f
Chris Lin <chris4540@gmail.com>
2019-04-24 23:33:31 +0200

    Update the concat script

--- concat_py.sh
+++ concat_py.sh
@@ -6,22 +6,39 @@ cat << EOF > ./submission_code.py
 # This file is for submission only. All related files are concatenated to this file for submission.
 EOF
 
+# Assign 1
+# files="
+# clsr/_base.py
+# clsr/one_layer_network.py
+# clsr/svm.py
+# lib_clsr/ann.py
+# lib_clsr/init.py
+# lib_clsr/svm.py
+# lib_clsr/utils.py
+# scripts/cmp_ann_svm.py
+# scripts/combine_all_skills.py
+# scripts/GridSearchCV_ann.py
+# scripts/plt_ex1_basic.py
+# scripts/run_1l_ann.py
+# scripts/run_svm.py
+# tests/test_ann_func.py
+# tests/test_svm_func.py
+# tests/utils.py
+# "
+
+# Assign 2
 files="
-clsr/_base.py
-clsr/one_layer_network.py
-clsr/svm.py
+clsr/two_layer_network.py
 lib_clsr/ann.py
 lib_clsr/init.py
-lib_clsr/svm.py
 lib_clsr/utils.py
-scripts/cmp_ann_svm.py
-scripts/combine_all_skills.py
-scripts/GridSearchCV_ann.py
-scripts/plt_ex1_basic.py
-scripts/run_1l_ann.py
-scripts/run_svm.py
-tests/test_ann_func.py
-tests/test_svm_func.py
+assign2/run_a2_ex3_f3.py
+assign2/run_a2_ex3_f4.py
+assign2/run_a2_ex4_best.py
+assign2/run_a2_ex4_get_search_val.py
+assign2/run_a2_ex4_search_lmbd.py
+tests/test_ann_2l.py
+tests/test_sanity_2l_ann.py
 tests/utils.py
 "
 
