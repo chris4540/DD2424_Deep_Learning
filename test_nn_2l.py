0b0fcd02387afe3d70c605aecfc91c850023db33
Chris Lin <chris4540@gmail.com>
2019-05-31 19:34:25 +0200

    Add updates

--- test_nn_2l.py
+++ test_nn_2l.py
@@ -1,8 +1,38 @@
+"""
+https://zhuanlan.zhihu.com/p/38200980
+"""
+import numpy as np
+from utils.load_batch import load_batch
+from utils.load_batch import cifar10_dataloader
+from utils.handle_data import data_split
+from utils.handle_data import get_all_train_data
+from utils.preprocess import StandardScaler
 from clsr.nn_2l import TwoLayerNeuralNetwork
 
 if __name__ == "__main__":
+    merged_data = get_all_train_data("cifar-10-batches-py")
+    train_data, valid_data = data_split(merged_data, n_valid=1000)
+    test_data = load_batch("cifar-10-batches-py/test_batch")
 
-    # make the network
-    clsr = TwoLayerNeuralNetwork()
-    # initialize the weights and bias
-    clsr.initalize_wgts()
+    scaler = StandardScaler()
+    scaler.fit(train_data['pixel_data'])
+    train_data['pixel_data'] = scaler.transform(train_data['pixel_data'])
+    valid_data['pixel_data'] = scaler.transform(valid_data['pixel_data'])
+    test_data['pixel_data'] = scaler.transform(test_data['pixel_data'])
+
+    # cnt = 0
+    # for inputs, labels in cifar10_dataloader(train_data, batch_size=100):
+    #     print(inputs.shape)
+    #     print(labels.shape)
+    #     cnt += labels.shape[0]
+    # print(cnt)
+    # print(type(train_data['labels']))
+    # print(train_data['labels'].shape)
+    # # print(test_data.keys())
+    # # print(train_data.keys())
+    # # print(valid_data.keys())
+    # # =================================================================
+    # # make the network
+    # # clsr = TwoLayerNeuralNetwork()
+    # # initialize the weights and bias
+    # # clsr.initalize_wgts()
