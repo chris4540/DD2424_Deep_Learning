1b8f65680ed7564130d47469dbeabdfb82612509
Chris Lin <chris4540@gmail.com>
2019-06-01 20:56:15 +0200

    Add jitter color

--- utils/arguments.py
+++ utils/arguments.py
@@ -6,13 +6,19 @@ Make some a small random geometric and photometric jitter
 
 See also:
 https://gombru.github.io/2017/09/14/data_augmentation/
+https://blog.csdn.net/tsq292978891/article/details/79107775
 """
 import numpy as np
 from scipy import ndimage
+from PIL import ImageEnhance
+from PIL import Image
 
 class ImageArgument:
 
     def __init__(self):
+        pass
+
+    def jitter(self, batchs):
         pass
 
     @staticmethod
@@ -41,10 +47,13 @@ class ImageArgument:
         return batch
 
     @staticmethod
-    def horrizonal_flip(imgs):
-        # flip the third axis
-        ret = imgs[:, :, ::-1, :]
-        return ret
+    def horrizonal_flip(img):
+        """
+        horrizonal flip the input img
+        """
+        # flip the second axis
+        flipped = img[:, ::-1, :]
+        return flipped
 
     @staticmethod
     def rotate(img, angle):
@@ -56,6 +65,28 @@ class ImageArgument:
         rotated = ndimage.rotate(img, angle, reshape=False, mode='nearest')
         assert rotated.shape == img.shape
         return rotated
+
+    @staticmethod
+    def add_gaussian_noisy(img, mean=0.05, sigma=0.03):
+        noisy = np.random.normal(mean, sigma, size=img.shape)
+        ret = img + noisy
+        return ret
+
+    @staticmethod
+    def jitter_color(img):
+        """
+        """
+        image = Image.fromarray(np.uint8(img*255))
+        random_factor = np.random.randint(5, 10) / 10.
+        color_image = ImageEnhance.Color(image).enhance(random_factor)
+        random_factor = np.random.randint(10, 21) / 10.
+        brightness_image = ImageEnhance.Brightness(color_image).enhance(random_factor)
+        random_factor = np.random.randint(10, 21) / 10.
+        contrast_image = ImageEnhance.Contrast(brightness_image).enhance(random_factor)
+        random_factor = np.random.randint(7, 10) / 10.
+        result_img = ImageEnhance.Sharpness(contrast_image).enhance(random_factor)
+        ret = np.asarray(result_img) / 255
+        return ret
 
 if __name__ == "__main__":
     pass
