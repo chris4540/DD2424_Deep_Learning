5c891cc2bb53df6b414f56167f207ddbeec121c1
Chris Lin <chris4540@gmail.com>
2019-06-05 01:46:33 +0200

    Code refactoring

--- utils/lrate.py
+++ utils/lrate.py
@@ -33,8 +33,43 @@ class CyclicLR:
     def get_lr(self):
         return self.lrates[self.idx]
 
-class Optimizer:
-    pass
+class AdaGradOptim:
+    """
+    AdaGrad optimization scheme. For RNN only now
+    """
 
-class AdaGrad:
-    pass
+    def __init__(self, model, lr=0.1):
+        self.model = model
+        self.params_names = model.parameters()
+        self.lr = lr
+        self.eps = np.finfo(model.dtype).eps
+        # the accumulative squared gradients
+        self._accu_sq_grad = dict()
+
+    def backward(self, logits, targets):
+        grads = self.model._get_backward_grad(logits, targets, clipping=True)
+
+        # update the momentum values
+        self._update_ada_grads(grads)
+
+        # update the model parameters with grads
+        self._backward_with_grads(grads)
+
+
+    def _update_ada_grads(self, grads):
+        for k in self.params_names:
+            grad_name = "grad_" + k
+            grad = grads[grad_name]
+            if k in self._accu_sq_grad:
+                self._accu_sq_grad[k] = self._accu_sq_grad[k] + grad**2
+            else:
+                # first time
+                self._accu_sq_grad[k] = grad**2
+
+    def _backward_with_grads(self, grads):
+        for k in self.params_names:
+            theta = getattr(self.model, k)  # obtain the parameter reference
+            grad = grads["grad_" + k]
+            sq_grad = self._accu_sq_grad[k]
+            update_amt = self.lr * grad / np.sqrt(sq_grad + self.eps)
+            theta -= update_amt
