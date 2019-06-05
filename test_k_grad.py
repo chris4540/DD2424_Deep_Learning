47afa23afd33226eff31d875477b519b57539d6e
Chris Lin <chris4540@gmail.com>
2019-06-05 18:21:02 +0200

    Add checking batch norm

--- test_k_grad.py
+++ test_k_grad.py
@@ -2,6 +2,7 @@ from utils.load_batch import cifar10_DataLoader
 from utils.load_batch import load_batch
 from clsr.nn_kl import KLayerNeuralNetwork
 import numpy as np
+from numpy.testing import assert_allclose
 
 def max_relative_err(grad1, grad2):
     eps = np.finfo(grad1.dtype).eps
@@ -9,6 +10,40 @@ def max_relative_err(grad1, grad2):
     demon = np.maximum(eps, np.abs(grad1) + np.abs(grad2))
     ret = dom / demon
     return np.max(ret)
+
+def check_num_grad(net, inputs, targets):
+    # calculate the gradient analytically
+    net.train()
+    out = net(inputs)
+    grads = net._get_backward_grad(out, targets, weight_decay=0.0)
+    # calculate the numerical gradient
+    h = 1e-5
+    h_inv = 1.0 / h
+    net.eval()
+    for param in net.parameters():
+        thetas = getattr(net, param)
+        for i, theta in enumerate(thetas):
+            grad_theta_num = np.zeros(theta.shape)
+            for idx in np.ndindex(theta.shape):
+                old_val = theta[idx]
+                theta[idx] = old_val - h
+                out = net(inputs)
+                l1 = net.cross_entropy(out, targets)
+
+                theta[idx] = old_val + h
+                out = net(inputs)
+                l2 = net.cross_entropy(out, targets)
+                grad = (l2-l1) * 0.5 * h_inv
+                grad_theta_num[idx] = grad
+            # -----------------------------------------
+            # obtain the analytical form
+            grad_an = grads['grad_' + param][i]
+            assert grad_an.shape == grad_theta_num.shape
+
+            # do some checking
+            name = "grad_%s.%d" % (param, i)
+            print(name, max_relative_err(grad_theta_num, grad_an))
+            # assert_allclose(grad_theta_num, grad_an, atol=1e-3, rtol=1e-4)
 
 
 if __name__ == "__main__":
@@ -29,34 +64,12 @@ if __name__ == "__main__":
     net = KLayerNeuralNetwork(
         p_dropout=0.0,
         n_features=10,
-        n_hidden_nodes=[10], batch_norm=False, dtype='float64')
-    net.train()
-    out = net(test_inputs)
-    grads = net._get_backward_grad(out, test_labels, weight_decay=0.0)
-    # ========================================================================
-    # print(grads)
-    # net.eval()
-    for param in net.parameters():
-        thetas = getattr(net, param)
-        for i, theta in enumerate(thetas):
-            grad_theta_num = np.zeros(theta.shape)
-            h = 1e-5
-            h_inv = 1.0 / h
-            for idx in np.ndindex(theta.shape):
-                old_val = theta[idx]
-                theta[idx] = old_val - h
-                out = net(test_inputs)
-                l1 = net.cross_entropy(out, test_labels)
+        n_hidden_nodes=[10, 10], batch_norm=False, dtype='float64')
+    check_num_grad(net, test_inputs, test_labels)
+    net2 = KLayerNeuralNetwork(
+        p_dropout=0.0,
+        n_features=10,
+        n_hidden_nodes=[10], batch_norm=True, dtype='float64')
 
-                theta[idx] = old_val + h
-                out = net(test_inputs)
-                l2 = net.cross_entropy(out, test_labels)
-                grad = (l2-l1) * 0.5 * h_inv
-                grad_theta_num[idx] = grad
-            # -----------------------------------------
-            # obtain the analytical form
-            grad_an = grads['grad_' + param][i]
-            assert grad_an.shape == grad_theta_num.shape
-            name = "grad_%s.%d" % (param, i)
-            print(name, max_relative_err(grad_theta_num, grad_an))
+    check_num_grad(net2, test_inputs, test_labels)
 
