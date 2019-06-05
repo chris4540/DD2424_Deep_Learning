82c60669cec93a7f82f37143bc55b314c2638207
Chris Lin <chris4540@gmail.com>
2019-06-05 10:54:15 +0200

    Add cost function csv

--- assign4/plt_basic_rnn_cost.sh
+++ assign4/plt_basic_rnn_cost.sh
@@ -2,6 +2,10 @@
 # For assignment 4 basic part
 
 echo "iteration,smooth_cost" > basic_cost.csv
-grep iteration rnn.log | awk '{print $2 "," $4}' >> basic_cost.csv
+grep iteration rnn_log.txt | awk '{print $2 "," $4}' >> basic_cost.csv
 
-python plt_csv.py basic_cost.csv
+# python plt_csv.py basic_cost.csv
+echo "iteration,smooth_cost" > trump_cost.csv
+grep iteration trump_tweet_log.txt | awk '{print $2 "," $4}' >> trump_cost.csv
+
+
