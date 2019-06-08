7e0b1ce06821c987bb8145a0e75e8af7ad7f40fa
Chris Lin <chris4540@gmail.com>
2019-06-08 21:30:36 +0800

    Add step learning rate

--- utils/lrate.py
+++ utils/lrate.py
@@ -1,4 +1,5 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
 def cyc_lrate(n_iters, eta_min=0, eta_max=1, step_size=5):
     iters = np.arange(n_iters)
@@ -7,7 +8,44 @@ def cyc_lrate(n_iters, eta_min=0, eta_max=1, step_size=5):
     eta_t = eta_min + (eta_max-eta_min)*np.maximum(0, (1-x))
     return eta_t
 
-class CyclicLR:
+class BaseOptimizer(ABC):
+
+
+    @abstractmethod
+    def step(self):
+        raise NotImplementedError("This method should be implemented by the inherited class")
+
+    @abstractmethod
+    def get_lr(self):
+        raise NotImplementedError("This method should be implemented by the inherited class")
+
+
+class StepLR(BaseOptimizer):
+    """
+    Mimic torch.optim.lr_scheduler.StepLR
+
+    Usage:
+    >>> scheduler = StepLR(0.05, 30, gamma=1/10)
+    >>> for input, labels in loader:
+            lrate = scheduler.get_lr()
+        scheduler.step()
+    """
+
+    def __init__(self, init_lr, step_size, gamma=0.1):
+        self.init_lr = init_lr
+        self.step_size = step_size
+        self.gamma = gamma
+        self.total_step = 0
+
+    def step(self):
+        self.total_step += 1
+
+    def get_lr(self):
+        n_step = self.total_step // self.step_size
+        ret = self.init_lr * ((self.gamma)**(-n_step))
+        return ret
+
+class CyclicLR(BaseOptimizer):
     """
     Mimic torch.optim.lr_scheduler.CyclicLR
 
