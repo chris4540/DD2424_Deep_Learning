5e3bf1444e9041376e7c44fec979e6306e3d2032
Chris Lin <chris4540@gmail.com>
2019-06-12 23:57:56 +0800

    Update sanity check

--- test_sanity_kl_nn.py
+++ test_sanity_kl_nn.py
@@ -14,23 +14,24 @@ from utils.load_batch import load_batch
 from utils.handle_data import data_split
 from utils import train
 from utils.lrate import StepLR
+from utils.lrate import CyclicLR
 from clsr.nn_kl import KLayerNeuralNetwork
 from utils.preprocess import StandardScaler
 
 if __name__ == "__main__":
     train_data = load_batch("cifar-10-batches-py/data_batch_1")
-    # net = KLayerNeuralNetwork(n_hidden_nodes=[100, 50], p_dropout=0.0, batch_norm=False)
-    net = KLayerNeuralNetwork(n_hidden_nodes=[80, 50], p_dropout=0.0, batch_norm=True, batch_norm_momentum=0.9)
+    net = KLayerNeuralNetwork(n_hidden_nodes=[50, 50], p_dropout=0.0, batch_norm=False, batch_norm_momentum=.9)
 
     scaler = StandardScaler()
     scaler.fit(train_data['pixel_data'])
     train_data['pixel_data'] = scaler.transform(train_data['pixel_data'])
 
     batch_size = 100
-    train_loader = cifar10_DataLoader(train_data, batch_size=batch_size)
+    train_loader = cifar10_DataLoader(train_data, batch_size=batch_size, shuffle=False)
 
     ntrain = train_data['labels'].shape[0]
-    n_epoch = 500
-    scheduler = StepLR(0.01, 30, gamma=.1)
+    n_epoch = 200
+    scheduler = StepLR(0.05, 20, gamma=.5)
+    # scheduler = CyclicLR(eta_min=1e-5, eta_max=1e-1, step_size=1000)
     for epoch in range(n_epoch):
         train(train_loader, net, 0.0, scheduler)
