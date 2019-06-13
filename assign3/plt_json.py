6b854d6eb9670a8fb09034b46314f3285ab16836
Chris Lin <chris4540@gmail.com>
2019-06-13 19:39:47 +0800

    Add search lambda

---
+++ assign3/plt_json.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+
+if __name__ == "__main__":
+    json_file = sys.argv[1]
+    json_name = json_file.split(".")[0]
+    df = pd.read_json(json_file)
+    df = df.set_index("weight_decay")
+    df = df.sort_index()
+    # print(df)
+
+    ax = df.plot(kind='line', legend=True, marker='x', logx=True)
+    ax.set_ylabel("Validation Acc.")
+    fig = ax.get_figure()
+    fig.savefig(json_name+'.png', bbox_inches='tight', dpi=200)
